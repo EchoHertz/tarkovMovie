607ddd2a75294e37dc4adc91d2c08d414cb6d96f
echo.youn <echo.youn@kakaocorp.com>
2020-11-06 14:50:10 +0900

    Update 경로 및 로그 키 변수 추가

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import datetime
 import pprint as pp
 
+raidName = "1게임 - 커스텀 0P 사망 (thorax)w2게임 - 커스텀 3P2U 탈출w3게임 - 커스텀 6U 탈출w4게임 - 커스텀 2P 탈출w5게임 - 랩 1R 사망 (head, eyes) w6게임 - 랩 1R 사망 (head, eyes)w7게임 - 쇼어라인 맴몸넘 탈출w8게임 - 랩 1P 사망 (head, top)w9게임 - 랩 0P 사망 (head, top)w10게임 - 랩 0P 사망 (thorax)w11게임 - 랩 0P 사망 (thorax)w12게임 - 랩 2R 토즈런 탈출".split('w')
+path = 'C:/Users/윤승재/Downloads/Logs'
+logKey = '2020.11.02_9-06-01_0.12.8.9660'
 
 # 첫번째 레이드 영상속 들어간 시간
 # PS.1:00:00 되기 전에 게임 들어가야함 ㅋㅋㅋㅋ
-firstTime = '17:35'
+firstTime = '18:00'
 
-applicationLog = open('log_2020.11.04_12-55-42_0.12.8.9660/2020.11.04_12-55-42_0.12.8.9660 application.log')
-notificationLog = open('log_2020.11.04_12-55-42_0.12.8.9660/2020.11.04_12-55-42_0.12.8.9660 notifications.log')
+applicationLog = open(path + '/log_' + logKey + '/' + logKey + ' application.log')
+notificationLog = open(path + '/log_' + logKey + '/' + logKey + ' notifications.log')
 
 startLogs = list(filter(lambda x: 'GameStarted' in x, applicationLog.readlines()))
 endLogs = list(filter(lambda x: 'UserMatchOver' in x, notificationLog.readlines()))
@@ -38,6 +41,8 @@ firstGame = datetime.datetime.strptime(startTimes[0], FMT) - datetime.timedelta(
 
 timeLine2 = ''
 timeLine = []
+print(len(startTimes))
+print(len(endTimes))
 if len(startTimes) == len(endTimes):
 
     for i in range(0, len(startTimes)):
@@ -45,7 +50,8 @@ if len(startTimes) == len(endTimes):
         e = datetime.datetime.strptime(endTimes[i], FMT)
         _durationSec = abs((s - e).total_seconds())
 
-        timeLine2 += (secToTime((s - firstGame).total_seconds()) + ' ~ ' + secToTime((e - firstGame).total_seconds()) + '\n')
+        timeLine2 += raidName[i] + (secToTime((s - firstGame).total_seconds()) + ' ~ ' + secToTime((e - firstGame).total_seconds()) + '\n')
+        # timeLine2 += (secToTime((s - firstGame).total_seconds()) + ' ~ ' + secToTime((e - firstGame).total_seconds()) + '\n')
 
         timeLine.append({
             "start": {
@@ -59,6 +65,7 @@ if len(startTimes) == len(endTimes):
             "durationSec": _durationSec.__trunc__(),
             "durationMin": _durationSec // 60,
             "nthGame": i + 1
+            # "raidTitle": raidName[i]
         })
 
 pp.pprint(timeLine, indent= 2, width=80, depth=10, compact= True, sort_dicts= False)
